2a369b91fb053a1862930f8387e8d5ce75da879a
Esha Patel <Patellesha11@gmail.com>
2025-04-02 11:40:55 +0300

    created array of objects

--- src/Components/button.js
+++ src/Components/button.js
@@ -2,9 +2,15 @@ import { useState } from "react";
 import './Button.css';
 
 function Button () {
+    
+    const pastel = [
+        {
+            
+        }
+    ]
     return (
         <div>
-            
+
         </div>
     )
 
