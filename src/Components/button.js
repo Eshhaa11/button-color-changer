e10ccc541596ecf8ca4ae5df97048891743c2bd7
Esha Patel <Patellesha11@gmail.com>
2025-04-02 11:47:33 +0300

    added

--- src/Components/button.js
+++ src/Components/button.js
@@ -4,8 +4,8 @@ import './Button.css';
 function Button () {
     
     const pastel = [
-        {name : "pink" , color: "#FFD1DC"}
-        {name : "green" , color: "#B4E7CE"}
+        {name: "pink" , color: "#FFD1DC"},
+        {name: "green", color: "#B4E7CE"},
     ]
     return (
         <div>
